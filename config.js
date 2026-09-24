702656f7b7f2c385b2f130945de9ad0ca91ecd94
ssrth18 <ssrth18@gmail.com>
2026-09-24 13:57:39 +0530

    Format LAUNCHPAD_CONFIG for better readability

--- config.js
+++ config.js
@@ -1,1 +1,4 @@
-window.LAUNCHPAD_CONFIG={supabaseUrl:"https://csyvxqpckwsffhxawulq.supabase.co",supabasePublishableKey:"sb_publishable_y6uTJCEs7ot7aeVfCMQCkA_zr0THWy1"};
+window.LAUNCHPAD_CONFIG = {
+  supabaseUrl: "https://csyvxqpckwsffhxawulq.supabase.co",
+  supabasePublishableKey: "sb_publishable_y6uTJCEs7ot7aeVfCMQCkA_zr0THWy1"
+};
